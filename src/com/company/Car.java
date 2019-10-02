13f578a19dc49331419973d2569ad87dc4d90a1f
java <anton.shikalyov@gmail.com>
2019-10-03 00:48:39 +0300

    Added Car class

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,6 +1,22 @@
 package com.company;
 
 public class Car {
-    public Car() {
+    String name;
+    String color;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
+
+    public Car(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+         Car mustang = new Car("mustang", "red" );
+         System.out.println(mustang);
     }
 }
